9f4259813501f6a170ecdaf07470925499053e9a
Smit <145379300+Smit1403@users.noreply.github.com>
2024-09-17 22:29:45 -0600

    string to braille works flawlessly

--- python/translator.py
+++ python/translator.py
@@ -1,75 +1,102 @@
 import sys
 import re
 
-capital_follows = ".....0"
-decimal_follows = ".0...0"
-num_follows = ".0.000"
+capital_follows = ".....O"
+decimal_follows = ".O...O"
+num_follows = ".O.OOO"
 special = {
 " " : "......",
-"." : "..00.0",
-"," : "..0...",
-"?" : "..0.00",
-"!" : "..000.",
-":" : "..00..",
-";" : "..0.0.",
-"-" : "....00",
-"/" : ".0..0.",
-"<" : ".00..0",
-">" : "0..00.",
-"(" : "0.0..0",
-")" : ".0.00."
+"." : "..OO.O",
+"," : "..O...",
+"?" : "..O.OO",
+"!" : "..OOO.",
+":" : "..OO..",
+";" : "..O.O.",
+"-" : "....OO",
+"/" : ".O..O.",
+"<" : ".OO..O",
+">" : "O..OO.",
+"(" : "O.O..O",
+")" : ".O.OO."
 }
 braille_int = {
-    1:"0.....",
-    2:"0.0...",
-    3:"00....",
-    4:"00.0..",
-    5:"0..0..",
-    6:"000...",
-    7:"0000..",
-    8:"0.00..",
-    9:".00...",
-    0:".000.."
+    "1":"O.....",
+    "2":"O.O...",
+    "3":"OO....",
+    "4":"OO.O..",
+    "5":"O..O..",
+    "6":"OOO...",
+    "7":"OOOO..",
+    "8":"O.OO..",
+    "9":".OO...",
+    "0":".OOO.."
                 }
 braille_char = {
-    'A':"0.....",
-    'B':"0.0...",
-    'C':"00....",
-    'D':"00.0..",
-    'E':"0..0..",
-    'F':"000...",
-    'G':"0000..",
-    'H':"0.00..",
-    'I':".00...",
-    'J':".000..",
-    'K':"0...0.",
-    'L':"0.0.0.",
-    'M':"00..0.",
-    'N':"00.00.",
-    'O':"0..00.",
-    'P':"000.0.",
-    'Q':"00000.",
-    'R':"0.000.",
-    'S':".00.0.",
-    'T':".0000.",
-    'U':"0...00",
-    'V':"0.0.00",
-    'W':".000.0",
-    'X':"00..00",
-    'Y':"00.000",
-    'Z':"0..000"
+    'A':"O.....",
+    'B':"O.O...",
+    'C':"OO....",
+    'D':"OO.O..",
+    'E':"O..O..",
+    'F':"OOO...",
+    'G':"OOOO..",
+    'H':"O.OO..",
+    'I':".OO...",
+    'J':".OOO..",
+    'K':"O...O.",
+    'L':"O.O.O.",
+    'M':"OO..O.",
+    'N':"OO.OO.",
+    'O':"O..OO.",
+    'P':"OOO.O.",
+    'Q':"OOOOO.",
+    'R':"O.OOO.",
+    'S':".OO.O.",
+    'T':".OOOO.",
+    'U':"O...OO",
+    'V':"O.O.OO",
+    'W':".OOO.O",
+    'X':"OO..OO",
+    'Y':"OO.OOO",
+    'Z':"O..OOO"
                 }
 
 retVal = []
 
 # FUNCTION TO GET STRING FROM BRAILLE
 def string_to_braille(str_val:str):
-    print(f"String to braile: {str_val}")
-    vals = list(str_val)
-    print(f"The string content: {vals}")
-    
+    addNum = False
+    addCapital = False
+    converted = []
+    # print(f"String to braile: {str_val}")
+    for i in range(len(str_val)):
+        if not str_val[i] in braille_char.keys():
+            try:
+                if str_val[i].capitalize() in braille_char.keys():
+                    converted.append(braille_char[str_val[i].capitalize()])
+            except:
+                pass
+        if str_val[i] in braille_char.keys():
+            converted.append(capital_follows)
+            converted.append(braille_char[str_val[i].capitalize()])
+        if str_val[i] in braille_int.keys() and addNum == False:
+            converted.append(num_follows)
+            converted.append(braille_int[str_val[i]])
+            addNum = True
+        elif str_val[i] in braille_int.keys() and addNum == True:
+            # converted.append(num_follows)
+            converted.append(braille_int[str_val[i]])
+        if str_val[i] in special.keys():
+            if str_val[i] == ".":
+                converted.append(decimal_follows)
+                converted.append(special["."])
+            else: converted.append(special[str_val[i]])
+        # print(converted)
+    retVal = "".join(converted)
+    print(retVal)
+            
 
 # FUNCTION TO GET BRAILLE FROM STRING
+# done
 def braille_to_string(str_val:str):
     print(f"Braile to string: {str_val}")
     vals = [str_val[i:i+6] for i in range(0,len(str_val), 6)]
@@ -82,14 +109,14 @@ if len(args) == 0:
     exit()
 
 str_val = " ".join(args)
-print(str_val)
+# print(str_val)
 
 try:
-    check_letters_int = any(char in braille_char.keys() for char in str_val) or any(char.capitalize() in braille_char.keys() for char in str_val) or any(int(char) in braille_int.keys() for char in str_val) 
+    check_letters_int = any(char.capitalize() in braille_char.keys() or char in braille_int.keys() for char in str_val) 
 except:
     check_letters_int = False
         
-print(check_letters_int)
+# print(check_letters_int)
 
 if check_letters_int:
     string_to_braille(str_val)
